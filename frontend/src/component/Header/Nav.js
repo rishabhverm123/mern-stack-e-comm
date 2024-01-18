fc597f46dafceeb4a97c7c29105c5bc3566d572c
Rishabh Verma <41505916+rishabhverm123@users.noreply.github.com>
2024-01-18 18:10:36 +0530

    logout functionality

--- frontend/src/component/Header/Nav.js
+++ frontend/src/component/Header/Nav.js
@@ -1,16 +1,25 @@
 
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import './Nav.css'
 export const Nav=()=>{
+    const navigate=useNavigate();
+    const logout=(e)=>{
+        e.preventDefault();
+        localStorage.clear();
+        navigate('/login')
+    }
     return(
         <div>
             <ul className='nav-ul'>
                 <li><Link to="/">Products</Link></li>
                 <li><Link to="/add">Add Product</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
-                <li><Link to="/logout">Logout</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
+                <li className='div_logout'>
+                   <a href='!#' onClick={(e)=>logout(e)}>Logout</a> 
+                </li>
             </ul>
+           
         </div>
     )
 }
